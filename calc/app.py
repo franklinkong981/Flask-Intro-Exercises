1876729396ab6468389c55c564f6740dff2311b5
Franklin Kong <franklinkong981@gmail.com>
2024-01-01 23:10:39 -0800

    Implemented calculator functions in calc folder

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,29 @@
-# Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+app = Flask(__name__)
+
+@app.route('/add')
+def add_response():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(add(a,b))
+
+@app.route('/sub')
+def sub_response():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(sub(a,b))
+
+@app.route('/mult')
+def mult_response():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(mult(a,b))
+
+@app.route('/div')
+def div_response():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(div(a,b))
+
+
